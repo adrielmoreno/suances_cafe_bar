f52d56fe577ab8bc1c68f080b0b4ada8d896a65c
MorenoCreativo <adrielmoreno@hotmail.com>
2024-11-28 23:30:15 +0100

    Fixed Overflow

--- lib/features/balance/presentation/widgets/expense_month_item.dart
+++ lib/features/balance/presentation/widgets/expense_month_item.dart
@@ -28,76 +28,80 @@ class ExpenseMonthItem extends StatelessWidget {
           Card(
             elevation: 3,
             child: MarginContainer(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: color.secondaryContainer,
-                            child: Icon(Icons.money_outlined,
-                                color: color.secondary),
-                          ),
-                          const SizedBox(width: Dimens.small),
-                          Text('Cash: \$${expense.cash.toStringAsFixed(2)}'),
-                        ],
-                      ),
-                      const SizedBox(height: Dimens.small),
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: color.primaryContainer,
-                            child: Icon(Icons.credit_card_rounded,
-                                color: color.primary),
-                          ),
-                          const SizedBox(width: Dimens.small),
-                          Text('Card: \$${expense.card.toStringAsFixed(2)}'),
-                        ],
-                      ),
-                      const SizedBox(height: Dimens.small),
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: color.tertiaryContainer,
-                            child: Icon(Icons.account_balance_outlined,
-                                color: color.tertiary),
-                          ),
-                          const SizedBox(width: Dimens.small),
-                          Text(
-                              'Transfer: \$${expense.transfer.toStringAsFixed(2)}'),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: Dimens.small, horizontal: Dimens.semiMedium),
-                    decoration: BoxDecoration(
-                      color: color.primary,
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Column(
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          'Total',
-                          style: theme.labelLarge?.copyWith(
-                              color: color.onPrimary,
-                              fontWeight: FontWeight.bold),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: color.secondaryContainer,
+                              child: Icon(Icons.money_outlined,
+                                  color: color.secondary),
+                            ),
+                            const SizedBox(width: Dimens.small),
+                            Text('Cash: \$${expense.cash.toStringAsFixed(2)}'),
+                          ],
                         ),
-                        const SizedBox(height: Dimens.extraSmall),
-                        Text(
-                          '\$${(expense.total).toStringAsFixed(2)}',
-                          style: theme.labelLarge?.copyWith(
-                              color: color.onPrimary,
-                              fontWeight: FontWeight.bold),
+                        const SizedBox(height: Dimens.small),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: color.primaryContainer,
+                              child: Icon(Icons.credit_card_rounded,
+                                  color: color.primary),
+                            ),
+                            const SizedBox(width: Dimens.small),
+                            Text('Card: \$${expense.card.toStringAsFixed(2)}'),
+                          ],
+                        ),
+                        const SizedBox(height: Dimens.small),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: color.tertiaryContainer,
+                              child: Icon(Icons.account_balance_outlined,
+                                  color: color.tertiary),
+                            ),
+                            const SizedBox(width: Dimens.small),
+                            Text(
+                                'Transfer: \$${expense.transfer.toStringAsFixed(2)}'),
+                          ],
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: Dimens.small,
+                          horizontal: Dimens.semiMedium),
+                      decoration: BoxDecoration(
+                        color: color.primary,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Total',
+                            style: theme.labelLarge?.copyWith(
+                                color: color.onPrimary,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: Dimens.extraSmall),
+                          Text(
+                            '\$${(expense.total).toStringAsFixed(2)}',
+                            style: theme.labelLarge?.copyWith(
+                                color: color.onPrimary,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
